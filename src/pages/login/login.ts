400b07aea4d5e998230e8b33b85d7c1c328d0265
diegofsousa <diegofernando5672@gmail.com>
2017-07-15 16:21:38 -0300

    add login with google

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {RegistrarPage} from "../registrar/registrar";
+import {LoginProvider} from "../../providers/login/login";
+import {Credencial} from "../../models/credencial";
 
 /**
  * Generated class for the LoginPage page.
@@ -15,12 +17,31 @@ import {RegistrarPage} from "../registrar/registrar";
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  credencial:Credencial;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public loginProvider: LoginProvider) {
+    this.credencial = new Credencial();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  ionViewDidLoad(){
+
+    this.loginProvider.loginSuccessEventEmitter.subscribe(
+      user => console.log(user)
+    );
+    this.loginProvider.loginFalhaEventEmitter.subscribe(
+      error => console.log(error)
+    );
   }
+
+  loginWithCredencial(){
+    this.loginProvider.loginComCredencial(this.credencial);
+  }
+
+  loginComGoogle(){
+    this.loginProvider.loginComGoogle();
+  }
+
   doRegister(){
     this.navCtrl.push(RegistrarPage);
   }
--- src/providers/login/login.ts
+++ src/providers/login/login.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable, NgZone} from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Credencial} from "../../models/credencial";
@@ -13,13 +13,63 @@ import firebase from "firebase";
 @Injectable()
 export class LoginProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello LoginProvider Provider');
+  currentUser:any;
+  autenticado:boolean;
+  loginSuccessEventEmitter:EventEmitter<any>;
+  loginFalhaEventEmitter:EventEmitter<any>;
+  logoutEventEmitter:EventEmitter<any>;
+
+  constructor(public http: Http, public ngZone: NgZone) {
+    this.loginSuccessEventEmitter = new EventEmitter();
+    this.loginFalhaEventEmitter = new EventEmitter();
+    this.loginFalhaEventEmitter = new EventEmitter();
+    firebase.auth().onAuthStateChanged(usuario => {
+      this.callbackStateChange(usuario);
+    })
+  }
+
+  private callbackStateChange(usuario){
+    this.ngZone.run( () =>{
+      if(usuario == null){
+        this.currentUser = null;
+        this.autenticado = false;
+      }else{
+        this.currentUser = usuario;
+        this.autenticado = true;
+      }
+    })
+  }
+
+  loginComCredencial(credencial:Credencial){
+    firebase.auth().signInWithEmailAndPassword(credencial.email, credencial.senha)
+      .then(result => this.callbackSucessLogin(result))
+      .catch(error => this.callbackFalhaLogin(error));
+  }
+
+  loginComGoogle(){
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => this.callbackSucessLogin(result))
+      .catch(error => this.callbackFalhaLogin(error));
+  }
+
+  sair(){
+    firebase.auth().signOut()
+      .then(() => this.logoutEventEmitter.emit(true))
+      .catch(error => this.callbackFalhaLogin(error))
   }
 
   registrarUsuario(credencial:Credencial){
     firebase.auth().createUserWithEmailAndPassword(credencial.email, credencial.senha)
       .then(result => console.log(result))
       .catch(error => console.log(error));
+  }
+
+  private callbackSucessLogin(response){
+    this.loginSuccessEventEmitter.emit(response.user);
+  }
+
+  private callbackFalhaLogin(error){
+    this.loginFalhaEventEmitter.emit({code : error.code, message: error.message, email: error.email, credencial: error.credencial});
   }
 }
